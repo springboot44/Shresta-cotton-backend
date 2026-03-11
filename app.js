65b2b8466a314f3e83ad09b75c5f9586c970d183
Durgam44 <springlearning44@gmail.com>
2026-03-11 16:41:42 +0530

    removed env file

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-
-import connectDB from "./src/config/db.js";
-import authRoutes from "./src/routes/AuthRoutes.js";
-import UserRoutes from "./src/routes/UserRoutes.js";
-import CottonRoutes from "./src/routes/CottonRoutes.js";
-import verifyToken from "./src/middleware/AuthMiddleWare.js";
-
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
+// import connectDB from './src/config/db.js';
+// import authRoutes from '/routes/AuthRoutes.js';
+// import UserRoutes from '/routes/UserRoutes.js';
+// import cors from 'cors';
+// import CottonRoutes from '/routes/CottonRoutes.js';
+
+import connectDB from './src/config/db.js';
+import authRoutes from './src/routes/AuthRoutes.js';
+import UserRoutes from './src/routes/UserRoutes.js';
+import cors from 'cors';
+import CottonRoutes from './src/routes/CottonRoutes.js';
+
+
+
+import verifyToken from './src/middleware/AuthMiddleWare.js';
 
 const app = express();
 
-// connect DB
+// Connect to MongoDB
 connectDB();
 
-// CORS FIRST
-app.use(cors({
-  origin: "*",
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"]
-}));
+// Middleware
+app.use(express.json()); // Parse incoming JSON
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
-app.options("*", cors());   // handle preflight
-
-// middleware
-app.use(express.json());
-
-// routes
-app.get("/", (req, res) => {
-  res.send("This is a test route");
+// Routes
+app.get('/', (req, res) => {
+  res.send('This is a test route');
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", verifyToken, UserRoutes);
-app.use("/api/cotton", CottonRoutes);
+app.use('/api/auth', authRoutes); // Auth routes
 
+app.use('/api/users', verifyToken, UserRoutes); // User routes
+
+app.use('/api/cotton', CottonRoutes); // Cotton routes
+// Start server
 const PORT = process.env.PORT || 3002;
-
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
